eba2b76a58514d5476db7e5f694285fa581fa8ab
Charles Bowen <charlie.b@teemill.com>
2020-06-29 14:15:07 +0100

    init

---
+++ src/Number.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Devshed\Arithmatic;
+
+class Number
+{
+    public int $value;
+
+    /**
+     * Number constructor.
+     *
+     * @param int $value
+     */
+    public function __construct(int $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param int $int
+     *
+     * @return $this
+     */
+    public function add(int $int)
+    {
+        $this->value += $int;
+
+        return $this;
+    }
+
+    /**
+     * @param int $int
+     *
+     * @return $this
+     */
+    public function subtract(int $int)
+    {
+        $this->value -= $int;
+
+        return $this;
+    }
+}
