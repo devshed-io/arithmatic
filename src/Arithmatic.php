0dfdb951f373c43bb6fe0cbef7d6cdf8292dcdc9
Charles Bowen <charlie.b@teemill.com>
2020-06-30 07:43:30 +0100

    feat: Added methods
    Output
    Divide
    Round
    percentageChange
    
    Added fluent instantiation methods: make,start
    
    Renamed package to be more sexy

---
+++ src/Arithmatic.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Devshed\Arithmatic;
+
+class Arithmatic
+{
+    protected $value;
+
+    /**
+     * Number constructor.
+     *
+     * @param int|float $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param int|float $value
+     *
+     * @return \Devshed\Arithmatic\Arithmatic
+     */
+    public static function start($value)
+    {
+        return new self($value);
+    }
+
+    /**
+     * @param int|float $value
+     *
+     * @return \Devshed\Arithmatic\Arithmatic
+     */
+    public static function make($value)
+    {
+        return self::start($value);
+    }
+
+    /**
+     * @param int|float $int
+     *
+     * @return \Devshed\Arithmatic\Arithmatic
+     */
+    public function add($int)
+    {
+        $this->value += $int;
+
+        return $this;
+    }
+
+    /**
+     * @param int|float $int
+     *
+     * @return \Devshed\Arithmatic\Arithmatic
+     */
+    public function subtract($int)
+    {
+        $this->value -= $int;
+
+        return $this;
+    }
+
+    /**
+     * @param int|float $int
+     *
+     * @return \Devshed\Arithmatic\Arithmatic
+     */
+    public function divide($int)
+    {
+        $this->value /= $int;
+
+        return $this;
+    }
+
+    /**
+     * @param int|float $from
+     *
+     * @return \Devshed\Arithmatic\Arithmatic
+     */
+    public function percentageChange($from)
+    {
+        $this->value = ($this->value - $from) / $from * 100;
+
+        return $this;
+    }
+
+    /**
+     * @param int $precision
+     * @param int $mode
+     *
+     * @return \Devshed\Arithmatic\Arithmatic
+     */
+    public function round(int $precision = 0, $mode = PHP_ROUND_HALF_UP)
+    {
+        $this->value = round($this->value, $precision, $mode);
+
+        return $this;
+    }
+
+    /**
+     * Output the value
+     *
+     * @return int|float
+     */
+    public function output()
+    {
+        return $this->value;
+    }
+}
---
+++ tests/unit/ArithmaticTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Devshed\Arithmatic\Arithmatic;
+
+it('starts with an integer', function () {
+    assertEquals(5, Arithmatic::start(5)->output());
+    assertEquals(10, Arithmatic::make(10)->output());
+});
+
+it('provides an output', function () {
+    $number = new Arithmatic(5);
+
+    assertTrue(method_exists($number, 'output'));
+});
+
+
+it('performs addition', function () {
+    $number = Arithmatic
+        ::start(5)
+        ->add(5);
+
+    assertEquals(10, $number->output());
+});
+
+it('performs subtraction', function () {
+    $number = Arithmatic
+        ::start(5)
+        ->subtract(2);
+
+    assertEquals(3, $number->output());
+});
+
+it('performs division', function () {
+    $number = Arithmatic::start(5)->divide(2);
+
+    assertEquals(2.5, $number->output());
+});
+
+it('performs percentage change', function () {
+    $number = Arithmatic::start(100)->percentageChange(80);
+
+    assertEquals(25.0, $number->output());
+
+    $number = Arithmatic::start(100)->percentageChange(125);
+
+    assertEquals(-20.0, $number->output());
+});
+
+it('performs rounding', function () {
+    $number = Arithmatic::start(1.5)->round();
+
+    assertEquals(2.0, $number->output());
+});
